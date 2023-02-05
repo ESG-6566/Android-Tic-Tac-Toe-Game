9f77d5b4c75b204292107f5d347cd8f596cfe62a
HEMASE-6566 <114760009+HEMASE-6566@users.noreply.github.com>
2023-02-05 17:47:54 +0330

    commit

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
